7edee3a436044a7d1e44c3510c6759656c8e7285
Arne De Smedt <arnedesmdt@gmail.com>
2020-03-20 08:37:24 +0100

    add hasAggregateRoot interface

---
+++ src/Aggregate/AggregateRootByNamespace.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\Aggregate;
+
+use ADS\Bundle\EventEngineBundle\Util;
+
+trait AggregateRootByNamespace
+{
+    /**
+     * @inheritDoc
+     */
+    public static function aggregateRoot()
+    {
+        Util::fromStateToAggregateClass(static::class);
+    }
+}
---
+++ src/Aggregate/HasAggregateRoot.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\Aggregate;
+
+interface HasAggregateRoot
+{
+    /**
+     * @return class-string|string
+     */
+    public static function aggregateRoot();
+}

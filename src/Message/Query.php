1c2c99a8e2a5b787dfa83bd7f8673b143d233d26
Arne De Smedt <arnedesmdt@gmail.com>
2020-03-30 13:32:16 +0200

    add query messages

--- src/Message/Query.php
+++ src/Message/Query.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Message;
 
+use EventEngine\Schema\ResponseTypeSchema;
+
 interface Query
 {
+    /**
+     * @return class-string|string
+     */
+    public static function __resolver();
+
+    /**
+     * @return array<int, ResponseTypeSchema>
+     */
+    public static function __responseSchemasPerStatusCode() : array;
+
+    public static function __defaultStatusCode() : int;
+
+    public static function __defaultResponseSchema() : ResponseTypeSchema;
 }

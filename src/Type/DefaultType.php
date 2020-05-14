aacdbff1827ae9d4a464ca8a5ed136ddb557c6fc
Arne De Smedt <arnedesmdt@gmail.com>
2020-05-14 09:41:54 +0200

    add empty response as type

--- src/Type/DefaultType.php
+++ src/Type/DefaultType.php
@@ -6,6 +6,7 @@ namespace ADS\Bundle\EventEngineBundle\Type;
 
 use EventEngine\JsonSchema\JsonSchema;
 use EventEngine\JsonSchema\Type\ArrayType;
+use EventEngine\JsonSchema\Type\ObjectType;
 use EventEngine\JsonSchema\Type\TypeRef;
 
 abstract class DefaultType
@@ -20,6 +21,11 @@ abstract class DefaultType
     public static function byId() : TypeRef
     {
         return JsonSchema::typeRef(static::typeRefName());
+    }
+
+    public static function emptyResponse() : ObjectType
+    {
+        return JsonSchema::object([]);
     }
 
     abstract public static function typeRefName() : string;

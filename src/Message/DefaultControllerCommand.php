a54b622df4153711c16f134222e9792d48ef8c01
Arne De Smedt <arnedesmdt@gmail.com>
2020-05-20 14:30:39 +0200

    no entity but controller in controller class

--- src/Message/DefaultControllerCommand.php
+++ src/Message/DefaultControllerCommand.php
@@ -7,7 +7,6 @@ namespace ADS\Bundle\EventEngineBundle\Message;
 use ADS\Bundle\EventEngineBundle\Exception\MessageException;
 use function array_pop;
 use function class_exists;
-use function count;
 use function explode;
 use function implode;
 use function sprintf;
@@ -17,11 +16,11 @@ trait DefaultControllerCommand
     public static function __controller() : string
     {
         $parts = explode('\\', static::class);
-        array_pop($parts);
+        $name = array_pop($parts);
         array_pop($parts);
         $namespace = implode('\\', $parts);
 
-        $controllerClass = sprintf('%s\\%s', $namespace, $parts[count($parts) - 1]);
+        $controllerClass = sprintf('%s\\Controller\\%s', $namespace, $name);
 
         if (! class_exists($controllerClass)) {
             throw MessageException::noControllerFound(static::class);

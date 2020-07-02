7a8db7cef235e111a0fb9dc96fd7edab047b2c2c
Arne De Smedt <arnedesmdt@gmail.com>
2020-07-02 12:40:49 +0200

    translate aggregate not found into notfoundhttp exception

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace ADS\Bundle\EventEngineBundle\Exception;
 
 use ADS\Bundle\EventEngineBundle\Util\StringUtil;
+use EventEngine\Aggregate\Exception\AggregateNotFound;
 use EventEngine\JsonSchema\Exception\JsonValidationError;
 use Prooph\EventStore\Exception\ConcurrencyException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 use function json_decode;
@@ -38,6 +40,14 @@ final class Handler implements EventSubscriberInterface
                 $event->setThrowable(
                     new ConflictHttpException(
                         'Resource already exists.',
+                        $exception
+                    )
+                );
+                break;
+            case $exception instanceof AggregateNotFound:
+                $event->setThrowable(
+                    new NotFoundHttpException(
+                        $exception->getMessage(),
                         $exception
                     )
                 );

ee4a8e0a8d2719d2be23b3fba962b48e00635582
Arne De Smedt <arnedesmdt@gmail.com>
2020-07-02 10:47:24 +0200

    add json validator exception in exception handler

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Exception;
 
+use ADS\Bundle\EventEngineBundle\Util\StringUtil;
+use EventEngine\JsonSchema\Exception\JsonValidationError;
 use Prooph\EventStore\Exception\ConcurrencyException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
+
+use function json_decode;
+use function json_encode;
+use function preg_match;
+use function sprintf;
+use function str_replace;
 
 final class Handler implements EventSubscriberInterface
 {
@@ -24,13 +33,35 @@ final class Handler implements EventSubscriberInterface
     {
         $exception = $event->getThrowable();
 
-        if (! $exception instanceof ConcurrencyException) {
-            return;
-        }
+        switch (true) {
+            case $exception instanceof ConcurrencyException:
+                $event->setThrowable(
+                    new ConflictHttpException(
+                        'Resource already exists.',
+                        $exception
+                    )
+                );
+                break;
+            case $exception instanceof JsonValidationError:
+                $message = $exception->getMessage();
 
-        throw new ConflictHttpException(
-            'Resource already exists.',
-            $exception
-        );
+                if (preg_match('/field "(.+)" \[(.+)\] ([.\S\s]+)$/m', $message, $matches)) {
+                    /** @var string $encodedJson */
+                    $encodedJson = json_encode(json_decode($matches[3], true));
+                    $message = sprintf(
+                        'Payload validation error: field \'%s\' [%s] %s',
+                        StringUtil::decamilize($matches[1]),
+                        $matches[2],
+                        str_replace('"', '\'', $encodedJson)
+                    );
+                }
+
+                $event->setThrowable(
+                    new BadRequestHttpException(
+                        $message,
+                        $exception
+                    )
+                );
+        }
     }
 }

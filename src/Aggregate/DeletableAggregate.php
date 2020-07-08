0129e56eb09fbdbe73ebb6434fa9096d96fc3123
Arne De Smedt <arnedesmdt@gmail.com>
2020-07-08 16:00:12 +0200

    add withDelete

--- src/Aggregate/DeletableAggregate.php
+++ src/Aggregate/DeletableAggregate.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Aggregate;
 
+/**
+ * @method static with(array $recordData)
+ */
 trait DeletableAggregate
 {
     private bool $deleted = false;
@@ -11,5 +14,15 @@ trait DeletableAggregate
     public function deleted(): bool
     {
         return $this->deleted === true;
+    }
+
+    /**
+     * @return static
+     */
+    public function delete()
+    {
+        $this->deleted = true;
+
+        return $this;
     }
 }

6817bf732cf5e2f5dbadd8dc605b7406c5753fb8
Siebe Vanden Eynden <siebe.vandeneynden@nucleus.be>
2021-01-21 11:39:39 +0100

    Added create projections table command

---
+++ src/SymfonyCommand/EventEngineProjectionsCreateCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\SymfonyCommand;
+
+use PDO;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class EventEngineProjectionsCreateCommand extends Command
+{
+    /**
+     * @var string
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint.MissingNativeTypeHint
+     */
+    protected static $defaultName = 'event-engine:projections:create';
+
+    private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        parent::__construct();
+
+        $this->connection = $connection;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Create the projections table.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->connection->exec(
+            'CREATE TABLE projections (
+  no BIGSERIAL,
+  name VARCHAR(150) NOT NULL,
+  position JSONB,
+  state JSONB,
+  status VARCHAR(28) NOT NULL,
+  locked_until CHAR(26),
+  PRIMARY KEY (no),
+  UNIQUE (name)
+);'
+        );
+
+        return Command::SUCCESS;
+    }
+}

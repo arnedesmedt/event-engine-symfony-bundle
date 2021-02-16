4ff0fb1c96c5ea78176cc2526271c725a81f4e8a
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-16 15:35:26 +0100

    fix return

--- src/Persistency/PDO.php
+++ src/Persistency/PDO.php
@@ -46,81 +46,94 @@ class PDO extends \PDO
         return true;
     }
 
-    public function prepare($query, $options = null)
+    public function prepare($query, $options = [])
     {
         $this->enableConnection();
-        parent::prepare($query, $options);
+
+        return parent::prepare($query, $options);
     }
 
     public function beginTransaction()
     {
         $this->enableConnection();
-        parent::beginTransaction();
+
+        return parent::beginTransaction();
     }
 
     public function commit()
     {
         $this->enableConnection();
-        parent::commit();
+
+        return parent::commit();
     }
 
     public function rollBack()
     {
         $this->enableConnection();
-        parent::rollBack();
+
+        return parent::rollBack();
     }
 
     public function inTransaction()
     {
         $this->enableConnection();
-        parent::inTransaction();
+
+        return parent::inTransaction();
     }
 
     public function exec($statement)
     {
         $this->enableConnection();
-        parent::exec($statement);
+
+        return parent::exec($statement);
     }
 
     public function query($statement, $mode = self::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = [])
     {
         $this->enableConnection();
-        parent::query($statement, $mode, $arg3, $ctorargs);
+
+        return parent::query($statement, $mode, $arg3, $ctorargs);
     }
 
     public function lastInsertId($name = null)
     {
         $this->enableConnection();
-        parent::lastInsertId($name);
+
+        return parent::lastInsertId($name);
     }
 
     public function errorCode()
     {
         $this->enableConnection();
-        parent::errorCode();
+
+        return parent::errorCode();
     }
 
     public function errorInfo()
     {
         $this->enableConnection();
-        parent::errorInfo();
+
+        return parent::errorInfo();
     }
 
     public function getAttribute($attribute)
     {
         $this->enableConnection();
-        parent::getAttribute($attribute);
+
+        return parent::getAttribute($attribute);
     }
 
     public function quote($string, $type = self::PARAM_STR)
     {
         $this->enableConnection();
-        parent::quote($string, $type);
+
+        return parent::quote($string, $type);
     }
 
     public function sqliteCreateFunction($function_name, $callback, $num_args = -1, $flags = 0)
     {
         $this->enableConnection();
-        parent::sqliteCreateFunction($function_name, $callback, $num_args, $flags);
+
+        return parent::sqliteCreateFunction($function_name, $callback, $num_args, $flags);
     }
 }

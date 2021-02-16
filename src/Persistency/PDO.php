698131bd912d3cedfbbff5584752f58148361d71
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-16 15:22:56 +0100

    phpcs for pdo

--- src/Persistency/PDO.php
+++ src/Persistency/PDO.php
@@ -25,7 +25,7 @@ class PDO extends \PDO
         ];
     }
 
-    public function enableConnection(): void
+    public function enableConnection()
     {
         if ($this->connected) {
             return;
@@ -46,79 +46,79 @@ class PDO extends \PDO
         return true;
     }
 
-    public function prepare($query, array $options = []): void
+    public function prepare($query, $options)
     {
         $this->enableConnection();
         parent::prepare($query, $options);
     }
 
-    public function beginTransaction(): void
+    public function beginTransaction()
     {
         $this->enableConnection();
         parent::beginTransaction();
     }
 
-    public function commit(): void
+    public function commit()
     {
         $this->enableConnection();
         parent::commit();
     }
 
-    public function rollBack(): void
+    public function rollBack()
     {
         $this->enableConnection();
         parent::rollBack();
     }
 
-    public function inTransaction(): void
+    public function inTransaction()
     {
         $this->enableConnection();
         parent::inTransaction();
     }
 
-    public function exec($statement): void
+    public function exec($statement)
     {
         $this->enableConnection();
         parent::exec($statement);
     }
 
-    public function query($statement, $mode = self::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = []): void
+    public function query($statement, $mode = self::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = [])
     {
         $this->enableConnection();
         parent::query($statement, $mode, $arg3, $ctorargs);
     }
 
-    public function lastInsertId($name = null): void
+    public function lastInsertId($name = null)
     {
         $this->enableConnection();
         parent::lastInsertId($name);
     }
 
-    public function errorCode(): void
+    public function errorCode()
     {
         $this->enableConnection();
         parent::errorCode();
     }
 
-    public function errorInfo(): void
+    public function errorInfo()
     {
         $this->enableConnection();
         parent::errorInfo();
     }
 
-    public function getAttribute($attribute): void
+    public function getAttribute($attribute)
     {
         $this->enableConnection();
         parent::getAttribute($attribute);
     }
 
-    public function quote($string, $type = self::PARAM_STR): void
+    public function quote($string, $type = self::PARAM_STR)
     {
         $this->enableConnection();
         parent::quote($string, $type);
     }
 
-    public function sqliteCreateFunction($function_name, $callback, $num_args = -1, $flags = 0): void
+    public function sqliteCreateFunction($function_name, $callback, $num_args = -1, $flags = 0)
     {
         $this->enableConnection();
         parent::sqliteCreateFunction($function_name, $callback, $num_args, $flags);

d97b82cb407592d7d8aab158071b86fca909ce83
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-16 15:13:58 +0100

    override all pdo functions

--- src/Persistency/PDO.php
+++ src/Persistency/PDO.php
@@ -39,12 +39,6 @@ class PDO extends \PDO
         $this->connected = true;
     }
 
-    /**
-     * @param mixed $value
-     *
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingAnyTypeHint
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingAnyTypeHint
-     */
     public function setAttribute($attribute, $value)
     {
         $this->attributes[$attribute] = $value;
@@ -52,15 +46,81 @@ class PDO extends \PDO
         return true;
     }
 
-    /**
-     * @param array<mixed> $arguments
-     *
-     * @return mixed
-     */
-    public function __call(string $name, array $arguments)
+    public function prepare($query, array $options = []): void
     {
         $this->enableConnection();
+        parent::prepare($query, $options);
+    }
 
-        return $this->{$name}(...$arguments);
+    public function beginTransaction(): void
+    {
+        $this->enableConnection();
+        parent::beginTransaction();
+    }
+
+    public function commit(): void
+    {
+        $this->enableConnection();
+        parent::commit();
+    }
+
+    public function rollBack(): void
+    {
+        $this->enableConnection();
+        parent::rollBack();
+    }
+
+    public function inTransaction(): void
+    {
+        $this->enableConnection();
+        parent::inTransaction();
+    }
+
+    public function exec($statement): void
+    {
+        $this->enableConnection();
+        parent::exec($statement);
+    }
+
+    public function query($statement, $mode = self::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = []): void
+    {
+        $this->enableConnection();
+        parent::query($statement, $mode, $arg3, $ctorargs);
+    }
+
+    public function lastInsertId($name = null): void
+    {
+        $this->enableConnection();
+        parent::lastInsertId($name);
+    }
+
+    public function errorCode(): void
+    {
+        $this->enableConnection();
+        parent::errorCode();
+    }
+
+    public function errorInfo(): void
+    {
+        $this->enableConnection();
+        parent::errorInfo();
+    }
+
+    public function getAttribute($attribute): void
+    {
+        $this->enableConnection();
+        parent::getAttribute($attribute);
+    }
+
+    public function quote($string, $type = self::PARAM_STR): void
+    {
+        $this->enableConnection();
+        parent::quote($string, $type);
+    }
+
+    public function sqliteCreateFunction($function_name, $callback, $num_args = -1, $flags = 0): void
+    {
+        $this->enableConnection();
+        parent::sqliteCreateFunction($function_name, $callback, $num_args, $flags);
     }
 }

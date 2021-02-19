0d5a96863cc45f2378be4226aa7c4e038370eea2
Arne De Smedt <arnedesmdt@gmail.com>
2021-02-19 08:31:44 +0100

    use json schema logic for type trait

--- src/Type/TypeNameIsClassName.php
+++ src/Type/TypeNameIsClassName.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Type;
 
+use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
 use ReflectionClass;
 
 trait TypeNameIsClassName
 {
+    use JsonSchemaAwareRecordLogic;
+
     public static function __type(): string
     {
         return (new ReflectionClass(static::class))->getShortName();

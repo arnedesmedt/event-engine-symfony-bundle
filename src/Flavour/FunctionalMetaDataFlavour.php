3efb868e0e959cc91eb3ad5d318a600313dea20a
Arne De Smedt <arnedesmdt@gmail.com>
2021-05-28 18:24:49 +0200

    add flavours that pushes metadata to the query resolvers

---
+++ src/Flavour/FunctionalMetaDataFlavour.php
@@ -0,0 +1,215 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\Flavour;
+
+use EventEngine\Messaging\Message;
+use EventEngine\Messaging\MessageBag;
+use EventEngine\Messaging\MessageFactory;
+use EventEngine\Messaging\MessageFactoryAware;
+use EventEngine\Runtime\Flavour;
+use EventEngine\Runtime\FunctionalFlavour;
+use Generator;
+use RuntimeException;
+
+class FunctionalMetaDataFlavour implements Flavour, MessageFactoryAware
+{
+    private FunctionalFlavour $functionalFlavour;
+
+    public function __construct(FunctionalFlavour $functionalFlavour)
+    {
+        $this->functionalFlavour = $functionalFlavour;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callCommandPreProcessor($preProcessor, Message $command)
+    {
+        return $this->functionalFlavour->callCommandPreProcessor($preProcessor, $command);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callCommandController($controller, Message $command)
+    {
+        return $this->functionalFlavour->callCommandController($controller, $command);
+    }
+
+    public function getAggregateIdFromCommand(string $aggregateIdPayloadKey, Message $command): string
+    {
+        return $this->functionalFlavour->getAggregateIdFromCommand($aggregateIdPayloadKey, $command);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callContextProvider($contextProvider, Message $command)
+    {
+        return $this->functionalFlavour->callContextProvider($contextProvider, $command);
+    }
+
+    /**
+     * @param mixed ...$contextServices
+     *
+     * @return Generator<mixed>
+     *
+     * @inheritDoc
+     */
+    public function callAggregateFactory(
+        string $aggregateType,
+        callable $aggregateFunction,
+        Message $command,
+        ...$contextServices
+    ): Generator {
+        return $this->functionalFlavour->callAggregateFactory(
+            $aggregateType,
+            $aggregateFunction,
+            $command,
+            ...$contextServices
+        );
+    }
+
+    /**
+     * @param mixed ...$contextServices
+     *
+     * @return Generator<mixed>
+     *
+     * @inheritDoc
+     */
+    public function callSubsequentAggregateFunction(
+        string $aggregateType,
+        callable $aggregateFunction,
+        $aggregateState,
+        Message $command,
+        ...$contextServices
+    ): Generator {
+        return $this->functionalFlavour->callSubsequentAggregateFunction(
+            $aggregateType,
+            $aggregateFunction,
+            $aggregateState,
+            $command,
+            ...$contextServices
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callApplyFirstEvent(callable $applyFunction, Message $event)
+    {
+        return $this->functionalFlavour->callApplyFirstEvent($applyFunction, $event);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callApplySubsequentEvent(callable $applyFunction, $aggregateState, Message $event)
+    {
+        return $this->functionalFlavour->callApplySubsequentEvent($applyFunction, $aggregateState, $event);
+    }
+
+    public function prepareNetworkTransmission(Message $message): Message
+    {
+        return $this->functionalFlavour->prepareNetworkTransmission($message);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertMessageReceivedFromNetwork(Message $message, $aggregateEvent = false): Message
+    {
+        return $this->functionalFlavour->convertMessageReceivedFromNetwork($message, $aggregateEvent);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callProjector($projector, string $projectionVersion, string $projectionName, Message $event): void
+    {
+        $this->functionalFlavour->callProjector($projector, $projectionVersion, $projectionName, $event);
+    }
+
+    /**
+     * @param mixed $aggregateState
+     *
+     * @return array<mixed>
+     *
+     * @inheritDoc
+     */
+    public function convertAggregateStateToArray(string $aggregateType, $aggregateState): array
+    {
+        return $this->functionalFlavour->convertAggregateStateToArray($aggregateType, $aggregateState);
+    }
+
+    public function canProvideAggregateMetadata(string $aggregateType): bool
+    {
+        return $this->functionalFlavour->canProvideAggregateMetadata($aggregateType);
+    }
+
+    /**
+     * @param mixed $aggregateState
+     *
+     * @return array<mixed>
+     *
+     * @inheritDoc
+     */
+    public function provideAggregateMetadata(string $aggregateType, int $version, $aggregateState): array
+    {
+        return $this->functionalFlavour->provideAggregateMetadata($aggregateType, $version, $aggregateState);
+    }
+
+    public function canBuildAggregateState(string $aggregateType): bool
+    {
+        return $this->functionalFlavour->canBuildAggregateState($aggregateType);
+    }
+
+    /**
+     * @param array<mixed> $state
+     *
+     * @inheritDoc
+     */
+    public function buildAggregateState(string $aggregateType, array $state, int $version)
+    {
+        return $this->functionalFlavour->buildAggregateState($aggregateType, $state, $version);
+    }
+
+    /**
+     * @return array<mixed>|Message|null
+     *
+     * @inheritDoc
+     */
+    public function callEventListener(callable $listener, Message $event)
+    {
+        return $this->functionalFlavour->callEventListener($listener, $event);
+    }
+
+    /**
+     * @param mixed $resolver
+     *
+     * @return mixed
+     *
+     * @inheritDoc
+     */
+    public function callQueryResolver($resolver, Message $query)
+    {
+        if (! $query instanceof MessageBag) {
+            throw new RuntimeException(
+                'Message passed to ' . __METHOD__ . ' should be of type ' . MessageBag::class
+            );
+        }
+
+        $queryMessage = $query->get(MessageBag::MESSAGE);
+        $filter = $query->getMetaOrDefault('filter', null);
+        $order = $query->getMetaOrDefault('order', null);
+
+        return $resolver($queryMessage, $filter, $order);
+    }
+
+    public function setMessageFactory(MessageFactory $messageFactory): void
+    {
+        $this->functionalFlavour->setMessageFactory($messageFactory);
+    }
+}
---
+++ src/Flavour/OopMetaDataFlavour.php
@@ -0,0 +1,205 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\Flavour;
+
+use EventEngine\Messaging\Message;
+use EventEngine\Messaging\MessageFactory;
+use EventEngine\Messaging\MessageFactoryAware;
+use EventEngine\Runtime\Flavour;
+use EventEngine\Runtime\OopFlavour;
+use Generator;
+
+final class OopMetaDataFlavour implements Flavour, MessageFactoryAware
+{
+    private OopFlavour $oopFlavour;
+    private FunctionalMetaDataFlavour $functionalMetaDataFlavour;
+
+    public function __construct(OopFlavour $oopFlavour, FunctionalMetaDataFlavour $functionalMetaDataFlavour)
+    {
+        $this->oopFlavour = $oopFlavour;
+        $this->functionalMetaDataFlavour = $functionalMetaDataFlavour;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callCommandPreProcessor($preProcessor, Message $command)
+    {
+        return $this->oopFlavour->callCommandPreProcessor($preProcessor, $command);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callCommandController($controller, Message $command)
+    {
+        return $this->oopFlavour->callCommandController($controller, $command);
+    }
+
+    public function getAggregateIdFromCommand(string $aggregateIdPayloadKey, Message $command): string
+    {
+        return $this->oopFlavour->getAggregateIdFromCommand($aggregateIdPayloadKey, $command);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callContextProvider($contextProvider, Message $command)
+    {
+        return $this->oopFlavour->callContextProvider($contextProvider, $command);
+    }
+
+    /**
+     * @param mixed ...$contextServices
+     *
+     * @return Generator<mixed>
+     *
+     * @inheritDoc
+     */
+    public function callAggregateFactory(
+        string $aggregateType,
+        callable $aggregateFunction,
+        Message $command,
+        ...$contextServices
+    ): Generator {
+        return $this->oopFlavour->callAggregateFactory(
+            $aggregateType,
+            $aggregateFunction,
+            $command,
+            ...$contextServices
+        );
+    }
+
+    /**
+     * @param mixed ...$contextServices
+     *
+     * @return Generator<mixed>
+     *
+     * @inheritDoc
+     */
+    public function callSubsequentAggregateFunction(
+        string $aggregateType,
+        callable $aggregateFunction,
+        $aggregateState,
+        Message $command,
+        ...$contextServices
+    ): Generator {
+        return $this->oopFlavour->callSubsequentAggregateFunction(
+            $aggregateType,
+            $aggregateFunction,
+            $aggregateState,
+            $command,
+            ...$contextServices
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callApplyFirstEvent(callable $applyFunction, Message $event)
+    {
+        return $this->oopFlavour->callApplyFirstEvent($applyFunction, $event);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callApplySubsequentEvent(callable $applyFunction, $aggregateState, Message $event)
+    {
+        return $this->oopFlavour->callApplySubsequentEvent($applyFunction, $aggregateState, $event);
+    }
+
+    public function prepareNetworkTransmission(Message $message): Message
+    {
+        return $this->oopFlavour->prepareNetworkTransmission($message);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function convertMessageReceivedFromNetwork(Message $message, $aggregateEvent = false): Message
+    {
+        return $this->oopFlavour->convertMessageReceivedFromNetwork($message, $aggregateEvent);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function callProjector($projector, string $projectionVersion, string $projectionName, Message $event): void
+    {
+        $this->oopFlavour->callProjector($projector, $projectionVersion, $projectionName, $event);
+    }
+
+    /**
+     * @param mixed $aggregateState
+     *
+     * @return array<mixed>
+     *
+     * @inheritDoc
+     */
+    public function convertAggregateStateToArray(string $aggregateType, $aggregateState): array
+    {
+        return $this->oopFlavour->convertAggregateStateToArray($aggregateType, $aggregateState);
+    }
+
+    public function canProvideAggregateMetadata(string $aggregateType): bool
+    {
+        return $this->oopFlavour->canProvideAggregateMetadata($aggregateType);
+    }
+
+    /**
+     * @param mixed $aggregateState
+     *
+     * @return array<mixed>
+     *
+     * @inheritDoc
+     */
+    public function provideAggregateMetadata(string $aggregateType, int $version, $aggregateState): array
+    {
+        return $this->oopFlavour->provideAggregateMetadata($aggregateType, $version, $aggregateState);
+    }
+
+    public function canBuildAggregateState(string $aggregateType): bool
+    {
+        return $this->oopFlavour->canBuildAggregateState($aggregateType);
+    }
+
+    /**
+     * @param array<mixed> $state
+     *
+     * @inheritDoc
+     */
+    public function buildAggregateState(string $aggregateType, array $state, int $version)
+    {
+        return $this->oopFlavour->buildAggregateState($aggregateType, $state, $version);
+    }
+
+    /**
+     * @return array<mixed>|Message|null
+     *
+     * @inheritDoc
+     */
+    public function callEventListener(callable $listener, Message $event)
+    {
+        return $this->oopFlavour->callEventListener($listener, $event);
+    }
+
+    /**
+     * @param mixed $resolver
+     *
+     * @return mixed
+     *
+     * @inheritDoc
+     */
+    public function callQueryResolver($resolver, Message $query)
+    {
+        return $this->functionalMetaDataFlavour->callQueryResolver($resolver, $query);
+    }
+
+    public function setMessageFactory(MessageFactory $messageFactory): void
+    {
+        $this->oopFlavour->setMessageFactory($messageFactory);
+    }
+}

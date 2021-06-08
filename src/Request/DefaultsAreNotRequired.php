9756687e1705ff04956a84dda0c0c32dc1e27a74
Arne De Smedt <arnedesmdt@gmail.com>
2021-06-08 11:55:26 +0200

    optionals properties are keys of default properties

--- src/Request/DefaultsAreNotRequired.php
+++ src/Request/DefaultsAreNotRequired.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace ADS\Bundle\EventEngineBundle\Request;
 
 use ADS\JsonImmutableObjects\JsonSchemaAwareRecordLogic;
-use ReflectionClass;
 
-use function array_filter;
 use function array_keys;
-use function in_array;
 
 trait DefaultsAreNotRequired
 {
@@ -22,13 +19,6 @@ trait DefaultsAreNotRequired
      */
     private static function __optionalProperties(): array
     {
-        $propertyNames = array_keys(self::buildPropTypeMap());
-
-        return array_filter(
-            array_keys(
-                (new ReflectionClass(static::class))->getDefaultProperties()
-            ),
-            static fn (string $propertyName) => in_array($propertyName, $propertyNames)
-        );
+        return array_keys(self::defaultProperties());
     }
 }

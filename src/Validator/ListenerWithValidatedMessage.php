086436c5e5c12232f6504d27690475c6b6e3dbce
Arne De Smedt <arnedesmdt@gmail.com>
2022-09-28 16:15:37 +0200

    fix di for listenerWithValidatedMessage

--- src/Validator/ListenerWithValidatedMessage.php
+++ src/Validator/ListenerWithValidatedMessage.php
@@ -6,10 +6,11 @@ namespace ADS\Bundle\EventEngineBundle\Validator;
 
 use ADS\Bundle\EventEngineBundle\Event\Listener;
 use Symfony\Component\Validator\Constraints\Valid;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
 
 abstract class ListenerWithValidatedMessage implements Listener
 {
-    public function __construct(protected SpecificationValidator $specificationValidator)
+    public function __construct(protected ConstraintValidatorInterface $specificationValidator)
     {
     }
 

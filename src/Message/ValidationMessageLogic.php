9309768721739c380cc42f498e03bc48f0522ccd
Arne De Smedt <arnedesmdt@gmail.com>
2022-10-20 21:23:18 +0200

    fix validation trait

--- src/Message/ValidationMessageLogic.php
+++ src/Message/ValidationMessageLogic.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace ADS\Bundle\EventEngineBundle\Message;
 
 use ADS\Bundle\EventEngineBundle\Validator\SpecificationValidation;
-use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 
 trait ValidationMessageLogic
@@ -20,14 +19,6 @@ trait ValidationMessageLogic
 
     public static function loadValidatorMetadata(ClassMetadata $metadata): void
     {
-        $metadata->addConstraint((new SpecificationValidation())->setCustomValidator(static::customValidator()));
-    }
-
-    /**
-     * @return class-string<ConstraintValidator>|null
-     */
-    protected static function customValidator(): ?string
-    {
-        return null;
+        $metadata->addConstraint(new SpecificationValidation());
     }
 }

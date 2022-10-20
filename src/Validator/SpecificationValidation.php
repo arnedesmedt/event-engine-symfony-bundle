a06c5e74dac2cc7750d6a8ef0bbf8ab1273740b0
Arne De Smedt <arnedesmdt@gmail.com>
2022-10-20 21:19:49 +0200

    specfication validator is not customizable

--- src/Validator/SpecificationValidation.php
+++ src/Validator/SpecificationValidation.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Validator;
 
-use ReflectionClass;
-use RuntimeException;
 use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\ConstraintValidatorInterface;
 
-use function sprintf;
-
-final class SpecificationValidation extends Constraint
+class SpecificationValidation extends Constraint
 {
-    private ?string $customValidatorClass = null;
-
     /**
      * @return string|array<string>
      */
@@ -25,29 +18,6 @@ final class SpecificationValidation extends Constraint
 
     public function validatedBy(): string
     {
-        return $this->customValidatorClass ?? SpecificationValidator::class;
-    }
-
-    /**
-     * @param class-string|null $customValidator
-     */
-    public function setCustomValidator(?string $customValidator): self
-    {
-        if (
-            $customValidator !== null
-            && ! (new ReflectionClass($customValidator))->implementsInterface(ConstraintValidatorInterface::class)
-        ) {
-            throw new RuntimeException(
-                sprintf(
-                    'Custom validator \'%s\', doesn\'t implement \'%s\'.',
-                    $customValidator,
-                    ConstraintValidatorInterface::class
-                )
-            );
-        }
-
-        $this->customValidatorClass = $customValidator;
-
-        return $this;
+        return SpecificationValidator::class;
     }
 }

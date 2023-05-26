101b451066479122edbaca6dd2adcb35007d2d9f
Arne De Smedt <arnedesmdt@gmail.com>
2023-05-26 09:07:07 +0200

    Finetune queueable message producer

--- src/Messenger/QueueableMessageProducer.php
+++ src/Messenger/QueueableMessageProducer.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Messenger;
 
+use EventEngine\EventEngine;
 use EventEngine\Messaging\Message;
 use EventEngine\Messaging\MessageBag;
+use EventEngine\Messaging\MessageDispatcher;
 use EventEngine\Messaging\MessageProducer;
+use EventEngine\Runtime\Flavour;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -15,13 +18,49 @@ use Throwable;
 
 use function reset;
 
-final class QueueableMessageProducer implements MessageProducer
+final class QueueableMessageProducer implements MessageProducer, MessageDispatcher
 {
     public function __construct(
         private readonly MessageBusInterface $commandBus,
         private readonly MessageBusInterface $eventBus,
         private readonly MessageBusInterface $queryBus,
+        private readonly Flavour $flavour,
+        private readonly EventEngine $eventEngine,
     ) {
+    }
+
+    /**
+     * @param array<string, mixed> $payload
+     * @param array<string, mixed> $metadata
+     *
+     * @throws Throwable
+     *
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingAnyTypeHint
+     */
+    public function dispatch($messageOrName, array $payload = [], array $metadata = []): mixed
+    {
+        if ($messageOrName instanceof Message) {
+            return $this->produce($messageOrName);
+        }
+
+        return $this->produce($this->messageBag($messageOrName, $payload, $metadata));
+    }
+
+    /**
+     * @param array<string, mixed> $payload
+     * @param array<string, mixed> $metadata
+     */
+    private function messageBag(string $messageClass, array $payload, array $metadata): Message
+    {
+        return $this->eventEngine
+            ->messageFactory()
+            ->createMessageFromArray(
+                $messageClass,
+                [
+                    'payload' => $payload,
+                    'metadata' => $metadata,
+                ],
+            );
     }
 
     public function produce(Message $message): mixed
@@ -34,7 +73,7 @@ final class QueueableMessageProducer implements MessageProducer
             // Since the message is not queueable, the transport won't be changed by PickTransportMiddleware.
             // Therefore we need to send the complete messageBag
             // so it contains the metadata with the async property set to true, and now the transport will be changed.
-            $innerMessage = $message;
+            $innerMessage = $this->flavour->prepareNetworkTransmission($message);
         }
 
         try {

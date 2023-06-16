909bcea8fbfd75b4d8e2f75c89a4e49f8aaa57d6
Arne De Smedt <arnedesmdt@gmail.com>
2023-06-16 15:19:42 +0200

    Update PickTransportMiddleware

--- src/Messenger/Middleware/PickTransportMiddleware.php
+++ src/Messenger/Middleware/PickTransportMiddleware.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Messenger\Middleware;
 
-use ADS\Bundle\EventEngineBundle\Command\Command;
-use ADS\Bundle\EventEngineBundle\Event\Event;
-use ADS\Bundle\EventEngineBundle\Message\Message;
 use ADS\Bundle\EventEngineBundle\Messenger\Queueable;
 use EventEngine\Messaging\Message as EventEngineMessage;
 use EventEngine\Messaging\MessageBag;
@@ -24,30 +21,13 @@ class PickTransportMiddleware implements MiddlewareInterface
 
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
-        $transport = null;
-        /** @var EventEngineMessage|Message $eventEngineMessage */
+        /** @var EventEngineMessage $eventEngineMessage */
         $eventEngineMessage = $envelope->getMessage();
-        $message = $eventEngineMessage instanceof Message
-            ? $eventEngineMessage
-            : $this->flavour->convertMessageReceivedFromNetwork($eventEngineMessage)->get(MessageBag::MESSAGE);
+        $message = $this->flavour->convertMessageReceivedFromNetwork($eventEngineMessage)->get(MessageBag::MESSAGE);
+        $sendAsync = $eventEngineMessage->getMetaOrDefault('async', null) ?? $message instanceof Queueable;
 
-        $eventEngineMessage = $eventEngineMessage instanceof EventEngineMessage ? $eventEngineMessage : null;
-        $sendAsync = $eventEngineMessage?->getMetaOrDefault('async', null) ?? $message instanceof Queueable;
-
-        if ($eventEngineMessage instanceof MessageBag && $sendAsync) {
-            $transport = $eventEngineMessage->messageType();
-        }
-
-        if ($message instanceof Queueable && $sendAsync) {
-            $transport = match (true) {
-                $message instanceof Command => 'command',
-                $message instanceof Event => 'event',
-                default => 'query',
-            };
-        }
-
-        if ($transport !== null) {
-            $envelope = $envelope->with(new TransportNamesStamp([$transport]));
+        if ($sendAsync) {
+            $envelope = $envelope->with(new TransportNamesStamp([$eventEngineMessage->messageType()]));
         }
 
         return $stack->next()->handle($envelope, $stack);

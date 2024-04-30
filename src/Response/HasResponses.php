ac0c05d80a302ffdad61e523499ea4e0e9a674e3
Arne De Smedt <arnedesmdt@gmail.com>
2024-04-30 12:19:10 +0200

    Response class can also be a collection

--- src/Response/HasResponses.php
+++ src/Response/HasResponses.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace ADS\Bundle\EventEngineBundle\Response;
 
+use EventEngine\JsonSchema\JsonSchemaAwareCollection;
 use EventEngine\JsonSchema\JsonSchemaAwareRecord;
 
 interface HasResponses
 {
-    /** @return class-string<JsonSchemaAwareRecord> */
+    /** @return class-string<JsonSchemaAwareRecord|JsonSchemaAwareCollection> */
     public static function __defaultResponseClass(): string;
 
     public static function __defaultStatusCode(): int;

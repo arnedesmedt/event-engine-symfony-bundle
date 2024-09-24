04487183aae0b14a8dd6e855fe0133d01136b8c8
Arne De Smedt <arnedesmdt@gmail.com>
2024-09-24 14:34:51 +0200

    Add event assertions

---
+++ src/TestHelper/EventAssertions.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ADS\Bundle\EventEngineBundle\TestHelper;
+
+use ADS\Bundle\EventEngineBundle\Event\Event;
+use ADS\Bundle\EventEngineBundle\Persistency\PDO;
+use ADS\ValueObjects\StringValue;
+use Closure;
+use PDOStatement;
+use Psr\Container\ContainerInterface;
+use TeamBlue\TestRequest\TestRequest\TestRequest;
+
+use function array_map;
+use function sprintf;
+
+class EventAssertions
+{
+    /** @param array<class-string<Event>> $listOfNewEventClasses */
+    private static function assertNewEventsForStream(
+        string $streamName,
+        StringValue $aggregateId,
+        array $listOfNewEventClasses,
+    ): Closure {
+        return function (TestRequest $testRequest) use ($aggregateId, $listOfNewEventClasses, $streamName): void {
+            /** @var ContainerInterface $container */
+            $container = static::getContainer();
+            /** @var PDO $connection */
+            $connection = $container->get('event_engine.connection');
+            /** @var PDOStatement $statement */
+            $statement = $connection->prepare(
+                sprintf(
+                    'SELECT * FROM %s_stream 
+                        WHERE 
+                            metadata->>\'_aggregate_id\' = :aggregateId AND 
+                            metadata->>\'_causation_id\' = :messageUuid 
+                        ORDER BY created_at',
+                    $streamName,
+                ),
+            );
+
+            $statement->execute(
+                [
+                    'aggregateId' => $aggregateId->toString(),
+                    'messageUuid' => $testRequest->requestId()->toString(),
+                ],
+            );
+            $newEventsInDatabase = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $newEventClassesInDatabase = array_map(
+                static fn (array $event) => $event['event_name'],
+                $newEventsInDatabase,
+            );
+
+            $this->assertEquals($listOfNewEventClasses, $newEventClassesInDatabase);
+        };
+    }
+}
